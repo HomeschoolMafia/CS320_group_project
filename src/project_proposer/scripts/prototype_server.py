66351a96fda48a48d4c8fc7b0f5df5bd3e74be08
creynolds <cayden@arclasr.net>
2020-01-28 02:34:37 -0500

    Ensured the entry point runs on Windows

--- src/project_proposer/scripts/prototype_server.py
+++ src/project_proposer/scripts/prototype_server.py
@@ -1,6 +1,8 @@
 import subprocess
-
+import platform
 
 def main():
-    subprocess.run(
-        "export FLASK_APP=src/project_proposer/prototype_server.py; flask run", shell=True)
+    if platform.system() == 'Windows':
+        subprocess.run('set FLASK_APP=src/project_proposer/prototype_server.py && flask run', shell=True)
+    else:
+        subprocess.run('export FLASK_APP=src/project_proposer/prototype_server.py && flask run', shell=True)
