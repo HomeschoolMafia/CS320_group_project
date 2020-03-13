a644351c0cc98f54706e37cefcaaa89ef7c641b5
Alexander Baldwin <abaldwin3@ycp.edu>
2020-03-13 19:32:20 -0400

    Change Branch Yea

--- src/ypd/scripts/prototype_server.py
+++ src/ypd/scripts/prototype_server.py
@@ -7,4 +7,4 @@ def main():
     if platform.system() == 'Windows':
         subprocess.run(f'set FLASK_APP={relative_path}/prototype_server.py && flask run', shell=True)
     else:
-        subprocess.run(f'export FLASK_APP={relative_path}/prototype_server.py && flask run -p 2000', shell=True)
+        subprocess.run(f'export FLASK_APP={relative_path}/prototype_server.py && flask run -p 2080', shell=True)
