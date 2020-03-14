c89ab15ae469ec61ee2b03f30aa2349b4ca9f3ca
Cayden Reynolds <cayden@arclasr.net>
2020-03-14 19:50:47 -0400

    added function to servlet class

--- src/ypd/prototype_server.py
+++ src/ypd/prototype_server.py
@@ -14,10 +14,6 @@ Base.metadata.create_all(engine)
 @app.route('/')
 def do_something():
     return 'Hello, world'
-
-@app.template_test('provided')
-def is_provided_filter(project):
-    return 1 if type(project) is Provided else 0
     
 SelectedProjectView.register(app)               # Imports the page after a project is selected
 SubmissionView.register(app)                    # Imports the page to submit a project
--- src/ypd/servlets/indexServlet.py
+++ src/ypd/servlets/indexServlet.py
@@ -1,11 +1,15 @@
-from flask import render_template
+from flask import render_template, current_app
 from flask_classy import FlaskView
 from ..model.catalog import Catalog
 from ..model.project import Provided
 
 class IndexView(FlaskView):
     def get(self):
+        current_app.jinja_env.tests['provided'] = self.is_provided_filter
         catalog = Catalog('', True)
         catalog.apply()
         return render_template('CS320-ProjectMainPage.html', catalog=catalog)
+    
+    def is_provided_filter(self, project):
+        return 1 if type(project) is Provided else 0
 
