cd8c9e1ab325ef8483efc3cea6691dd4e1d2e108
Alexander Baldwin <abaldwin3@ycp.edu>
2020-03-14 18:43:01 -0400

    Change NavBar

--- src/ypd/servlets/indexServlet.py
+++ src/ypd/servlets/indexServlet.py
@@ -6,5 +6,6 @@ class IndexView(FlaskView):
     def get(self):
         catalog = Catalog('', True)
         catalog.apply()
+        search_text = request.args.get('search_text', default='')
         return render_template('CS320-ProjectMainPage.html', catalog=catalog)
     
