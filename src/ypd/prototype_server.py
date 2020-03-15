ae16a2ce831191647e2c97e04f8e4e1753b7090d
Gabriel <gholley@ycp.edu>
2020-03-14 22:14:31 -0400

    Made some slight changes to Submission Servlet

--- src/ypd/prototype_server.py
+++ src/ypd/prototype_server.py
@@ -7,8 +7,10 @@ from .servlets.selectedProjectServlet import SelectedProjectView
 from flask_classy import FlaskView
 from .model import engine, Base
 
+
 IndexView.register(app)
 Base.metadata.create_all(engine)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def do_something():
--- src/ypd/servlets/submissionServlet.py
+++ src/ypd/servlets/submissionServlet.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_classy import FlaskView
+from flask_classy import FlaskView, route
 from ..model.project import Provided, Solicited
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextField, SubmitField, RadioField
@@ -7,24 +7,26 @@ from wtforms.validators import DataRequired
 
 class SubmissionView(FlaskView):
     def get(self):
-        print ('You Win')
         return render_template('SubmissionPage.html')
-    
-    # pull data from HTML form
+
+        # pull data from HTML form
     def post(self):
         form = SubmissionForm()
-        if form.validate_on_submit():
-            return redirect(url_for('success'))
-        return render_template('SubmissionPage.html', form = form)
+        projType = form.projType.data
+        title = form.title.data
+        description = form.description.data
+        poster = form.poster.data
 
+        if form.validate_on_submit():
     # post data to project database
-        if projType == 'providedProject':
-            provided = Provided()
-            provided.post(title, description, poster)
-        else:
-            solicited = Solicited()
-            print ('Hello')
-            solicited.post(title, description, poster)
+            if projType == 'providedProject':
+                provided = Provided()
+                provided.post(title, description, poster)
+            else:
+                solicited = Solicited()
+                solicited.post(title, description, poster)
+            
+        return redirect(url_for('/submission', form=form))
 
 class SubmissionForm(FlaskForm):
     """Submission Form"""
