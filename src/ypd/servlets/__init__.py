fbdfd93557de313d729bbfece3fb3986af514c07
Cayden Reynolds <cayden@arclasr.net>
2020-04-01 14:19:22 -0400

    Should actually work on windows now

--- src/ypd/servlets/__init__.py
+++ src/ypd/servlets/__init__.py
@@ -7,6 +7,6 @@ import glob
 modules = Path(__file__).parent.glob('*.py')
 __all__ = []
 for module in modules:
-    module_name = str(module).rpartition('.')[0].rpartition('/')[2]
+    module_name = module.as_posix().rpartition('.')[0].rpartition('/')[2]
     if module_name != '__init__':
         __all__.append(module_name)
