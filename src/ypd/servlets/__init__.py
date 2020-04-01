606f4394ffc6f539354d47beb6a506dcf035b9e9
Cayden Reynolds <cayden@arclasr.net>
2020-04-01 13:58:51 -0400

    removed extra parenthesis

--- src/ypd/servlets/__init__.py
+++ src/ypd/servlets/__init__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import glob
 
 #Load all .py files in this directory so we can import them all at once in server.py
-modules = (Path(__file__).parent.glob('*.py'))
+modules = Path(__file__).parent.glob('*.py')
 __all__ = []
 for module in modules:
     module_name = str(module).rpartition('.')[0].rpartition('/')[2]
