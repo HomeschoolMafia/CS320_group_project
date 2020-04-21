8674176237f23abbc87387ebf1343ef386943878
Alexander Baldwin <abaldwin3@ycp.edu>
2020-04-21 15:51:52 -0400

    Fixed Comments

--- src/ypd/scripts/loadcsv.py
+++
@@ -1,9 +0,0 @@
-import csv 
-
-from ypd import relative_path
-
-def main():
-    with open(f'{relative_path}/db/project.csv','r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            print(row)
