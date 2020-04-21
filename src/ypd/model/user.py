556451320427ec2dfad5c067db6e39ed5814be96
QuietCoder1007 <llewis9@ycp.edu>
2020-04-21 15:25:41 -0400

    Fixed dat sexy csv

--- src/ypd/model/user.py
+++ src/ypd/model/user.py
@@ -281,4 +281,3 @@ class User(Base, DBModel, HasFavoritesMixin, UserMixin):
            session.query(Provided).filter_by(id=acc.id).delete()
 
         session.delete(acc)
-        session.delete(self)
